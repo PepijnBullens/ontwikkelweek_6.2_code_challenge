d9f3d0705a97cdcc5620d42c06f6a5e33492c399
PepijnBullens <pepijnpersoonlijk@gmail.com>
2025-01-27 11:44:30 +0100

    added part 10

---
+++ 10/script.js
@@ -0,0 +1,12 @@
+function checkPrimeNumber(input) {
+  if (input < 2) {
+    return false;
+  }
+
+  for (let i = 2; i < input; i++) {
+    if (input % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
